7cd055e228cf5432335278d4542ec2c1da8701c7
Vitali Baliankou <trivium.exe@gmail.com>
2021-06-25 21:27:29 +0300

    dialogs

--- src/Redux/dialogs_reducer.tsx
+++ src/Redux/dialogs_reducer.tsx
@@ -65,7 +65,6 @@ const dialogsReducer = (state: InitialStateType = initialState, action: DialogAc
     }
 }
 export const sendMessageAC = (): SendMessageType => ({type: SEND_MESSAGE})
-
 export const updateNewMessageTextAC = (text: string): UpdateNewMessageTextType => ({
     type: UPDATE_NEW_MESSAGE_TEXT, newMessage: text
 })
