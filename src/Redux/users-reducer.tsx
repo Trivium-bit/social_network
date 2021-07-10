5f85254d887f4aa06d019bfb24f13d0fb7ec8778
Vitali Baliankou <trivium.exe@gmail.com>
2021-07-10 22:37:31 +0300

    auth_reducer

--- src/Redux/users-reducer.tsx
+++ src/Redux/users-reducer.tsx
@@ -73,6 +73,7 @@ const SET_USERS = "SET_USERS"
 const SET_CURRENT_PAGE = "SET_CURRENT_PAGE"
 const SET_TOTAL_USERS_COUNT = "SET_TOTAL_USERS_COUNT"
 const TOGGLE_IS_FETCHING = "TOGGLE_IS_FETCHING"
+
 const TOGGLE_IS_FOLLOWING_PROGRESS = "TOGGLE_IS_FOLLOWING_PROGRESS"
 
 const usersReducer = (state: InitialStateType = initialState, action: UsersActionsType) => {
