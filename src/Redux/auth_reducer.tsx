45583d655bf41624120a21d6d87c8ff0d37bc74b
Vitali Baliankou <trivium.exe@gmail.com>
2021-07-18 21:54:37 +0300

    auth_reducer

--- src/Redux/auth_reducer.tsx
+++ src/Redux/auth_reducer.tsx
@@ -49,6 +49,7 @@ export const setAuthUserData = (id: number, email: string, login: string) => ({
     type: SET_USER_DATA,
     data: {id, email, login}
 })
+
 export const getAuthUserData = () => (dispatch: any) => {
     authAPI.me().then((response: any) => {
         if (response.data.resultCode === 0) {
