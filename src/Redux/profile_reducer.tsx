dce55c991cb376cbe82b16cd8b362521bdc30efa
Trivium-bit <trivium.exe@gmail.com>
2021-11-13 10:55:34 +0300

    getStatus

--- src/Redux/profile_reducer.tsx
+++ src/Redux/profile_reducer.tsx
@@ -1,5 +1,5 @@
-import {usersAPI} from "../api/api";
-import {ProfileType} from "../components/Profile/ProfileContainer";
+import { usersAPI, profileAPI } from "../api/api";
+import { ProfileType } from "../components/Profile/ProfileContainer";
 import { AppActionsType } from "./redux-store";
 
 export type PostsType = {
@@ -16,29 +16,35 @@ export type UpdateNewPostTextActionType = {
     newText: string
 }
 
+export type SetStatusActionType = {
+    type: typeof SET_STATUS
+    status: string
+}
+
 export type SetUserProfileActionType = {
     type: typeof SET_USER_PROFILE
     profile: ProfileType
 }
 
-const ADD_POST = 'ADD-POST'
-const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
-const SET_USER_PROFILE = 'SET_USER_PROFILE'
-
-
 export type ProfilePageType = {
     posts: Array<PostsType>
     newPostText: string
     profile: ProfileType
+    status: string
 }
+
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const SET_USER_PROFILE = 'SET_USER_PROFILE';
+const SET_STATUS = 'SET_STATUS';
 
 
 export const profileInitialState: ProfilePageType = {
     posts: [
-        {id: 1, message: "Hi how are you?", likesCount: 12},
-        {id: 2, message: "Hi!", likesCount: 9},
-        {id: 3, message: "Go Dota?", likesCount: 50},
-        {id: 4, message: "Yo!", likesCount: 15}
+        { id: 1, message: "Hi how are you?", likesCount: 12 },
+        { id: 2, message: "Hi!", likesCount: 9 },
+        { id: 3, message: "Go Dota?", likesCount: 50 },
+        { id: 4, message: "Yo!", likesCount: 15 }
     ] as Array<PostsType>,
     newPostText: 'it-kamasutra.com',
     profile: {
@@ -46,15 +52,16 @@ export const profileInitialState: ProfilePageType = {
         lookingForAJob: false,
         lookingForAJobDescription: 'string',
         fullName: 'string'
-    }
+    },
+    status: 'string'
 }
 
-export type ProfileActionsType = AddPostActionType | UpdateNewPostTextActionType | SetUserProfileActionType
+export type ProfileActionsType = AddPostActionType | UpdateNewPostTextActionType | SetUserProfileActionType | SetStatusActionType
 
 const profileReducer = (state: ProfilePageType = profileInitialState, action: AppActionsType): ProfilePageType => {
     switch (action.type) {
-       case ADD_POST: {
-            let newPost: PostsType = {id: 5,message: state.newPostText,likesCount: 0};
+        case ADD_POST: {
+            let newPost: PostsType = { id: 5, message: state.newPostText, likesCount: 0 };
             return {
                 ...state,
                 posts: [...state.posts, newPost],
@@ -67,8 +74,14 @@ const profileReducer = (state: ProfilePageType = profileInitialState, action: Ap
                 newPostText: action.newText
             };
         }
+        case SET_STATUS: {
+            return {
+                ...state,
+                status: action.status
+            };
+        }
         case SET_USER_PROFILE: {
-            return {...state, profile: action.profile};
+            return { ...state, profile: action.profile };
         }
         default:
             return state;
@@ -76,13 +89,36 @@ const profileReducer = (state: ProfilePageType = profileInitialState, action: Ap
 }
 
 export const addPostActionCreator = (): AddPostActionType => ({
-    type: ADD_POST});
+    type: ADD_POST
+});
 export const updateNewPostTextActionCreator = (newPostText: string): UpdateNewPostTextActionType => ({
-    type: UPDATE_NEW_POST_TEXT, newText: newPostText});
+    type: UPDATE_NEW_POST_TEXT, newText: newPostText
+});
 export const setUserProfile = (profile: ProfileType): SetUserProfileActionType => ({
-    type: SET_USER_PROFILE, profile});
+    type: SET_USER_PROFILE, profile
+});
+export const setStatus = (status: string): SetStatusActionType => ({
+    type: SET_STATUS, status
+});
+
+
+export const getStatus = (userId: number) => (dispatch: any) => {
+    profileAPI.getStatus(userId)
+    .then(response => {
+        dispatch(setStatus(response.data));
+    })
+}
+
+export const updateStatus = (status: string) => (dispatch: any) => {
+    profileAPI.updateStatus(status)
+    .then(response => {
+        dispatch(setStatus(response.data));
+    })
+}
+
 export const getUserProfile = (userId: number) => (dispatch: any) => {
-    usersAPI.getProfile(userId).then(response => {
+    usersAPI.getProfile(userId)
+    .then(response => {
         dispatch(setUserProfile(response.data));
     })
 }
