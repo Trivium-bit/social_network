0d11f736805f3c0bf5094caaf86892ebb65c0937
Trivium-bit <trivium.exe@gmail.com>
2021-11-17 10:12:26 +0300

    Fields

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,30 +1,35 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form'
 
 const LoginForm = () => {
     return (
-    <form>
-    <div>
-        <input placeholder={"Login"}/>
-     </div>  
-    <div>
-    <input placeholder={"Password"}/>
-        </div>  
-    <div>
-    <input type={"checkbox"}/> remember me
-    </div>  
-    <div>
-        <button>Login</button>
-        </div>  
+        <form>
+            <div>
+                <Field placeholder={"Login"} name={"login"} component={"input"} />
+            </div>
+            <div>
+                <Field placeholder={"Password"} name={"password"} component={"input"} />
+            </div>
+            <div>
+                <Field component={"input"} name={"remember me"} type={"checkbox"} /> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
         </form>
-        ) 
-    }    
+    )
+}
+
+const LoginReduxFrom = reduxForm({
+    form: 'login'
+})(LoginForm)
 
 const Login = () => {
     return <div>
         <h1>Login</h1>
-       <LoginForm />
- </div>
-    
+        <LoginReduxFrom />
+    </div>
+
 
 
 
