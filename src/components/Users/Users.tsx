946c97536321de88d44fcd43ccc7e26203942da8
Trivium-bit <trivium.exe@gmail.com>
2021-11-27 08:29:04 +0300

    currentPage

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './users.module.css';
-import {UsersType} from "../../Redux/users-reducer";
+import { UsersType } from "../../Redux/users-reducer";
 import default_ava from '../../assets/images/default_ava.jpg'
 
 type PropsType = {
@@ -28,45 +28,47 @@ let Users = (props: PropsType) => {
         <div>
             {pages.map(p => {
                 return <span className={props.currentPage === p ? styles.selectedPage : ''}
-                             onClick={() => {
-                                 props.onPageChanged(p)
-                             }}>{p}</span>
+                    onClick={() => {
+                        props.onPageChanged(p)
+                    }}>{p}</span>
             })}
         </div>
         {
             props.users.map((u: any) => <div key={u.id}>
-                    <span>
-                        <div>
-                            <img src={u.photos.small !== null ? u.photos.small : default_ava} alt={"avatar"}
-                                 className={styles.userPhoto}/>
-                        </div>
-                    </span>
                 <span>
-                        <div>
-                            {u.followed                               ? <button disabled={props.followingInProgress
-                                    .some(id => id === u.id)}
-                                    onClick={() => {props.unfollow(u.id)
-                                }}> Unfollow </button>
+                    <div>
+                        <img src={u.photos.small !== null ? u.photos.small : default_ava} alt={"avatar"}
+                            className={styles.userPhoto} />
+                    </div>
+                </span>
+                <span>
+                    <div>
+                        {u.followed ? <button disabled={props.followingInProgress
+                            .some(id => id === u.id)}
+                            onClick={() => {
+                                props.unfollow(u.id)
+                            }}> Unfollow </button>
 
-                                : <button disabled={props.followingInProgress
-                                    .some(id => id === u.id)} onClick={() => {
-                                    props.follow(u.id)}}
-                                >Follow </button>
-                            }
-                        </div>
-                    </span>
+                            : <button disabled={props.followingInProgress
+                                .some(id => id === u.id)} onClick={() => {
+                                    props.follow(u.id)
+                                }}
+                            >Follow </button>
+                        }
+                    </div>
+                </span>
                 <span>
-                        <div>
-                            <span>
-                                <div> {u.name} </div>
-                                <div> {u.status} </div>
-                            </span>
-                            <span>
-                                <div> {"u.location.country"} </div>
-                                <div> {"u.location.city"} </div>
-                            </span>
-                        </div>
-                    </span>
+                    <div>
+                        <span>
+                            <div> {u.name} </div>
+                            <div> {u.status} </div>
+                        </span>
+                        <span>
+                            <div> {"u.location.country"} </div>
+                            <div> {"u.location.city"} </div>
+                        </span>
+                    </div>
+                </span>
             </div>)
         }
     </div>
