b7036ff9337d87ae1bed15ffcb176611f546b301
Trivium-bit <trivium.exe@gmail.com>
2022-02-09 07:28:34 +0300

    ProfileInfo_avatar

--- src/components/Profile/Myposts/Post/Post.tsx
+++ src/components/Profile/Myposts/Post/Post.tsx
@@ -8,10 +8,9 @@ export type PostsType = {
 }
 
 const Post = (props: PostsType) =>{
-
     return (      
                     <div className={classes.post}>
-                    <img alt="" src="https://shutniki.club/wp-content/uploads/2019/12/v1-babay29.png"/>
+                    <img alt="avatar" src="https://shutniki.club/wp-content/uploads/2019/12/v1-babay29.png"/>
                          {props.message}
                          <div>like {props.likesCount}</div>
                          </div>                      
--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -17,9 +17,11 @@ const ProfileInfo = (props: PropsType) => {
     }
     return (
         <div className={classes.content}>
-           <img src='https://the-flow.ru/uploads/images/resize/830x0/adaptiveResize/11/27/10/38/62/fbf218a40cd7.jpg'/>
-            <div className={classes.discriptionBlock}>
-                ava+description
+            <div><img src='https://the-flow.ru/uploads/images/resize/830x0/adaptiveResize/11/27/10/38/62/fbf218a40cd7.jpg'/></div>
+                <div className={classes.discriptionBlock}>
+                <span><img className={classes.img} src='https://avatars.mds.yandex.net/i?id=6769d84f1f7e2020125db3dde0c3bef4-5876270-images-thumbs&n=13' alt="profile Avatar"/></span>
+                <span>Trivium It's best killer in CS:GO</span>
+             
                 <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
                 </div>
         </div>
