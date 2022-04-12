f2b64436e0cbb82e30b28b98f27e02e3627c30e6
Trivium-bit <Trivium.exe@gmail.com>
2022-04-12 22:19:18 +0300

    paginator

--- src/components/common/Paginator/Paginator.tsx
+++ src/components/common/Paginator/Paginator.tsx
@@ -1,39 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './paginator.module.css';
 import { setUsers, UsersType } from "../../../Redux/users-reducer";
 import default_ava from '../../assets/images/default_ava.jpg'
 import axios from 'axios';
 
 type PropsType = {
-    totalUsersCount: number
+    totalItemsCount: number
     pageSize: number
     currentPage: number
+    portionSize: number
     onPageChanged: (pageNumber: number) => void
 }
 
-export const Paginator = ({totalUsersCount, pageSize, currentPage, onPageChanged, ...props}: PropsType) => {
-   /*  if(props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-        .then(response => {
-            props.setUsers(response.data.items);
-        });
-    }
- */
-    let pagesCount = Math.ceil(totalUsersCount / pageSize);
+export const Paginator = ({ totalItemsCount, pageSize, currentPage, onPageChanged, portionSize = 10, ...props }: PropsType) => {
+
+    let pagesCount = Math.ceil(totalItemsCount / pageSize);
 
     let pages = [];
     for (let i = 0; i <= pagesCount; i++) {
         pages.push(i);
     }
-    return <div>
-            {pages.map(p => {
-                return <span className={currentPage === p ? styles.selectedPage : ''}
-                    onClick={() => {
+
+    let portionCount = Math.ceil(pagesCount / portionSize);
+    let [portionNumber, setPortionNumber] = useState(1);
+    let leftPortionPageNuber = (portionNumber - 1) * portionSize + 1;
+    let rightPortionPageNuber = portionNumber * portionSize;
+
+    return <div className={styles.paginator}>
+        {portionNumber > 1 && <button onClick={() => { setPortionNumber(portionNumber - 1) }}>prev</button>}
+        {pages
+            .filter(p => p >= leftPortionPageNuber && p <= rightPortionPageNuber)
+            .map((p) => {
+                return <span className={cn({
+                    [styles.selectedPage]: currentPage === p
+                }, styles.pageNuber)}
+                    key={p}
+                    onClick={(e) => {
                         onPageChanged(p)
                     }}>{p}</span>
             })}
-        </div>
- 
+        {portionCount > portionNumber &&
+            <button onClick={() => { setPortionNumber(portionNumber + 1) }}>prev</button>}
+    </div>
+
 }
 
 export default Paginator
