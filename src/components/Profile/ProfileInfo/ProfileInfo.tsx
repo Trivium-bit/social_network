c4c0ff5a5ea71401d3cd5a62df875113c6f35138
Trivium-bit <Trivium.exe@gmail.com>
2022-05-19 22:16:09 +0300

    profileData, contacts

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,7 +1,15 @@
+import { useState } from 'react';
 import { ProfileType } from '../../../Redux/profile_reducer';
 import Preloader from '../../common/Preloader/Preloader';
+import ProfileData from './ProfileData';
 import classes from './ProfileInfo.module.css';
 import ProfileStatusWithHooks from './ProfileStatusWithHooks';
+
+type ProfileData = {
+    profile: ProfileType
+    isOwner: boolean
+    goToEditMode: boolean
+}
 
 type PropsType = {
     profile: ProfileType
@@ -12,6 +20,9 @@ type PropsType = {
 }
 
 const ProfileInfo = ({ profile, status, updateStatus, isOwner, savePhoto }: PropsType) => {
+
+    let [editMode, setEditMode] = useState(false);
+
     if (!profile) {
         return <Preloader />
     }
@@ -22,37 +33,18 @@ const ProfileInfo = ({ profile, status, updateStatus, isOwner, savePhoto }: Prop
         }
 
     }
-    debugger
+
     return (
         <div className={classes.content}>
             <div className={classes.discriptionBlock}>
                 <span><img className={classes.ava} src={profile.photos.large || 'https://avatars.mds.yandex.net/i?id=6769d84f1f7e2020125db3dde0c3bef4-5876270-images-thumbs&n=13'} alt="ava" /></span>
                 {isOwner && <input type={"file"} onChange={onMainPhotoSelected} />}
                 <span>I love this game</span>
-
-                <div>
-                    <div>
-                        <b>Full name</b> : {profile.fullName ? "yes" : "no"}
-                    </div>
-                    <div>
-                        <b>Looking for a job</b> : {profile.lookingForAJob ? "yes" : "no"}
-                    </div>
-                    {profile.lookingForAJob &&
-                        <div>
-                            <b>My professional skills</b> : {profile.lookingForAJobDescription}
-                        </div>
-                    }
-                     <div>
-                        <b>About me</b> : {profile.abotMe}
-                    </div>
-                    <div>
-{/*                         <b>Contacts</b> : {profile} */}
-                    </div>
-                </div>
-
+                {editMode
+                    ? <ProfileDataForm />
+                    : <ProfileData goToEditMode={() => { setEditMode(true) }} profile={profile} isOwner={isOwner} />}
                 <ProfileStatusWithHooks status={status} updateStatus={updateStatus} />
             </div>
         </div>
     )
 }
-export default ProfileInfo;
